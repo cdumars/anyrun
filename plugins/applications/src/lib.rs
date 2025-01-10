37a0c0ae455ebda95639fbe3a303895849f3e96a
Cooper Dumars <cooper@dumars.org>
2025-01-09 18:53:32 -0800

    add a `app_launch_prefix` config option to applications plugin

--- plugins/applications/src/lib.rs
+++ plugins/applications/src/lib.rs
@@ -10,6 +10,7 @@ pub struct Config {
     desktop_actions: bool,
     max_entries: usize,
     terminal: Option<Terminal>,
+    app_launch_prefix: Option<String>,
 }
 
 #[derive(Deserialize)]
@@ -24,6 +25,7 @@ impl Default for Config {
             desktop_actions: false,
             max_entries: 5,
             terminal: None,
+            app_launch_prefix: None,
         }
     }
 }
@@ -112,19 +114,37 @@ pub fn handler(selection: Match, state: &State) -> HandleResult {
     } else if let Err(why) = {
         let current_dir = &env::current_dir().unwrap();
 
-        Command::new("sh")
-            .arg("-c")
-            .arg(&entry.exec)
-            .current_dir(if let Some(path) = &entry.path {
-                if path.exists() {
-                    path
+        eprintln!("{:?}", state.config.app_launch_prefix);
+
+        if let Some(prefix) = state.config.app_launch_prefix.as_ref() {
+            Command::new("sh")
+                .arg("-c")
+                .arg(format!("{} {}", prefix, &entry.exec))
+                .current_dir(if let Some(path) = &entry.path {
+                    if path.exists() {
+                        path
+                    } else {
+                        current_dir
+                    }
                 } else {
                     current_dir
-                }
-            } else {
-                current_dir
-            })
-            .spawn()
+                })
+                .spawn()
+        } else {
+            Command::new("sh")
+                .arg("-c")
+                .arg(&entry.exec)
+                .current_dir(if let Some(path) = &entry.path {
+                    if path.exists() {
+                        path
+                    } else {
+                        current_dir
+                    }
+                } else {
+                    current_dir
+                })
+                .spawn()
+        }
     } {
         eprintln!("Error running desktop entry: {}", why);
     }
